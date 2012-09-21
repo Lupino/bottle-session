8acbdb156437393a4e1befafa5c7816a91c247e4
Ubasic <lmjubuntu@gmail.com>
2012-09-21 15:16:04 +0800

    Add bottle_session.py

---
+++ bottle_session.py
@@ -0,0 +1,36 @@
+__author__ = "Li Meng Jun"
+__version__ = '0.0.1'
+__license__ = 'MIT'
+
+### CUT HERE (see setup.py)
+
+from bottle import request
+
+
+class SessionPlugin(object):
+    '''
+    '''
+    name = 'session'
+
+    def __init__(self):
+        self.app = None
+
+    def setup(self, app):
+        '''
+        Make sure that other installed plugins don't affect the same keyword argument.
+        '''
+        self.app = app
+        self.app.hooks.add('before_request', self.load_session)
+        self.app.hooks.add('after_request', self.set_session)
+        
+    def load_session(self):
+        self.app.session = request.environ.get('beaker.session', {})
+
+    def set_session(self):
+        pass
+
+    def apply(self, callback, context):
+        return callback
+
+
+Plugin = SessionPlugin
